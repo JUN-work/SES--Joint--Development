42e0ad5a71d9ae176c4eed5eff102046f6512532
AYA <ice6e63@gmail.com>
2020-10-04 11:41:18 +0900

    DB情報を別ファイルに退避

--- englishStudy_laravel/workspace/SES-Joint-Development/quizJSchoise_DB/index.php
+++ englishStudy_laravel/workspace/SES-Joint-Development/quizJSchoise_DB/index.php
@@ -24,19 +24,7 @@ $monme=$_SESSION['monme'];
   }
 
 //DB情報の設定・接続チェック
-  define('DB_HOST', 'mysql');
-  define('DB_USER', 'default');
-  define('DB_PASSWORD', 'root');
-  define('DB_NAME', 'mysql');
-  try {
-    $dsn = 'mysql:host='.DB_HOST.'; dbname='.DB_NAME.';charset=utf8;';
-    $pdo = new PDO($dsn, DB_USER, DB_PASSWORD);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-  }
-  catch(PDOException $e){
-    print('接続エラーが発生しました。:'.$e->getMessage());
-    exit;
-  }
+require dirname(__FILE__) . '/dbinfo.php';
 
 //選択された問題の情報を取得、取得した問題のカラムに入っている答え(answer)を正解としてセット。
 $sql="SELECT question,answer,explanation,url FROM js_questions WHERE id=:id;";
