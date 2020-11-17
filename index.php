4696f01b1259e6bb08da58b36570182431ac378d
JUN-work <yume.debutan.8255@gmail.com>
2020-11-17 18:14:56 +0900

    トップページのフロント面修正

--- index.php
+++ index.php
@@ -5,19 +5,22 @@
   >
     <meta charset="UTF-8" />
     <title>SE2 -Study English Site for Engineers-</title>
-    <meta name="description" content="プログラミング言語別に英語が学べるサイトです" />
+    <meta 
+      name="description" 
+      content="プログラミング言語別に英語が学べるサイトです" 
+    />
     <meta
       name="viewport"
       content="width=device-width, initial-scale=1, shrink-to-fit=no"
     />
-    <link rel="canonical" href="あなたのサイトURL" />
+    <link rel="canonical" href="https://ses-joint-development.herokuapp.com/" />
     <link rel="icon" type="image/png" href="ファビコンのパス" />
     <!-- OGP設定 -->
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="あなたのサイトURL" />
-    <meta property="og:image" content="SNSで表示させたい画像のパス" />
-    <meta property="og:title" content="ページタイトル" />
-    <meta property="og:description" content="サイトの説明文" />
+    <meta property="og:type" content="SE2-Site" />
+    <meta property="og:url" content="https://ses-joint-development.herokuapp.com/" />
+    <meta property="og:image" content="img/SE2.png" />
+    <meta property="og:title" content="SE2 -Study English Site for Engineers-" />
+    <meta property="og:description" content="プログラミング言語別に英語が学べるサイトです" />
     <!-- スタイルシートはここから -->
     <link
       rel="stylesheet"
@@ -44,17 +47,31 @@
 
   <body id="page-top">
     <!----------------------------------------------->
-    <header>
-      <nav>
-        <div class="container navbar navbar-expand-lg navbar-light">
-          <a class="navbar-brand mr-auto" href="index.php">
-            <img src="img/SE2.png" alt="サイト名" height="70"/>
-          </a>
-          <a href="register.php" class="btn btn-secondary btn-lg mr-1 text-white">新規登録</a>
-          <a href="login.php" class="btn btn-primary btn-lg mr-1 text-white">ログイン</a>
-        </div>
-      </nav>
-    </header>
+    <div id="global-container">
+      <div id="container">
+        <div class="mobile-menu__cover"></div>
+        <header class="header">
+          <div class="header__inner">
+            <div class="logo">
+              <a href="index.php"><img src="img/SE2.png" alt="SE2" /></a>
+            </div>
+            <nav class="header__nav">
+              <ul class="header__ul">
+                <li class="btn btn-outline-dark btn-lg mr-1 header__li">
+                  <a href="register.php">新規登録</a>
+                </li>
+                <li class="btn btn-info mr-1 btn-lg header__li">
+                  <a href="login.php">ログイン</a>
+                </li>
+              </ul>
+            </nav>
+            <button class="mobile-menu__btn">
+              <span></span>
+              <span></span>
+              <span></span>
+            </button>
+          </div>
+        </header>
     <!----------------------------------------------->
     <main>
       <div class="main-top">
@@ -68,7 +85,6 @@
             CSS
           </button>
         </div>
-
         <div class="questions__select">
           <div class="questions__type">
             <span>選択式</span>
@@ -80,7 +96,7 @@
           <div class="questions__type">
             <span>記述式</span>
             <div class="questions__start">
-              <a href="#" class="btn btn-default btn-lg">学習する</a>
+              <a href="#" class="btn btn-default btn-lg">準備中</a>
             </div>
           </div>
         </div>
@@ -100,7 +116,7 @@
           <div class="questions__type">
             <span>記述式</span>
             <div class="questions__start">
-              <a href="#" class="btn btn-default btn-lg">学習する</a>
+              <a href="#" class="btn btn-default btn-lg">準備中</a>
             </div>
           </div>
         </div>
@@ -111,55 +127,117 @@
     <!----------------------------------------------->
     <footer id="footer" class="bg-light py-5">
       <!--display-4→display-5、py-5→py-1に変更-->
-      <h2 class="display-5 text-center font-patrick py-1">Production Staff's SNS</h2>
+      <h2 class="display-5 text-center font-patrick py-1">
+        Production Staff's SNS
+      </h2>
       <div class="container py-4 py-md-5">
         
     
         <div id="footer-index" class="row pt-1">
           <!--pt-4→pt-1に変更-->
           <ul class="col list-unstyled">
-            <li>JUN</li>
+            <li>＊JUN</li>
             <li>
               <ul class="list-unstyled">
-                <li><a href="https://twitter.com/JUN_work_life" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-                <li><a href="https://github.com/JUN-work" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                <li>
+                  <a 
+                  href="https://twitter.com/JUN_work_life" 
+                  target="_blank" 
+                  rel="noopener noreferrer">
+                    Twitter
+                  </a>
+                </li>
+                <li>
+                  <a 
+                  href="https://github.com/JUN-work" 
+                  target="_blank" 
+                  rel="noopener noreferrer">
+                    GitHub
+                  </a>
+                </li>
               </ul>
             </li>
           </ul>
           <ul class="col list-unstyled">
-            <li>Yuki.Ishizaki</li>
+            <li>＊Yuki.Ishizaki</li>
             <li>
               <ul class="list-unstyled">
-                <li><a href="https://twitter.com/zakky0519" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-                <li><a href="https://github.com/YukiIshizaki0525" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                <li>
+                  <a 
+                  href="https://twitter.com/zakky0519" 
+                  target="_blank" 
+                  rel="noopener noreferrer">
+                    Twitter
+                  </a>
+                </li>
+                <li>
+                  <a 
+                  href="https://github.com/YukiIshizaki0525" 
+                  target="_blank" 
+                  rel="noopener noreferrer">
+                    GitHub
+                  </a>
+                </li>
               </ul>
             </li>
           </ul>
           <ul class="col list-unstyled">
-            <li>そってぃ</li>
+            <li>＊そってぃ</li>
             <li>
               <ul class="list-unstyled">
-                <li><a href="https://twitter.com/sohhprog" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-                <li><a href="https://github.com/sohh85" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                <li>
+                  <a 
+                  href="https://twitter.com/sohhprog" 
+                  target="_blank" 
+                  rel="noopener noreferrer">
+                    Twitter
+                  </a>
+                </li>
+                <li>
+                  <a 
+                  href="https://github.com/sohh85" 
+                  target="_blank" 
+                  rel="noopener noreferrer">
+                    GitHub
+                  </a>
+                </li>
               </ul>
             </li>
           </ul>
           <ul class="col list-unstyled">
-            <li>綾</li>
+            <li>＊綾</li>
             <li>
               <ul class="list-unstyled">
-                <li><a href="https://twitter.com/yanagi4939" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-                <li><a href="https://github.com/AYA-kurokiri09" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                <li>
+                  <a 
+                  href="https://twitter.com/yanagi4939" 
+                  target="_blank" rel="noopener noreferrer">
+                    Twitter
+                  </a>
+                </li>
+                <li>
+                  <a 
+                  href="https://github.com/AYA-kurokiri09" 
+                  target="_blank" 
+                  rel="noopener noreferrer">
+                    GitHub
+                  </a>
+                </li>
               </ul>
             </li>
           </ul>
           <ul class="col list-unstyled">
-            <li><button type="button" class="btn btn-light">
-              <a href="./contact/contact_top.php">お問い合わせはこちら</a></button>
+            <li>
+              <button type="button" id="contact_btn" class="btn btn-info">
+              <a href="./contact/contact_top.php" class="contact_font">お問い合わせはこちら</a>
+            </button>
             </li>
+            <!--contactフォルダ内のcontact_top.php(お問い合わせページテンプレ)に飛びます-->
           </ul>
-        </div><!-- /.footer-index -->
-      </div><!-- .container -->
+        </div>
+        <!-- /.footer-index -->
+      </div>
+      <!-- .container -->
     </footer>
 
     <div class="text-white bg-primary">
@@ -174,24 +252,43 @@
       </div>
     </div>
 
+      <nav class="mobile-menu">
+        <div class="logo">
+          <img src="img/SE2.png" alt="SE2" />
+        </div>
+        <ul class="mobile-menu__main">
+          <li class="mobile-menu__item">
+            <a href="register.php" class="mobile-menu__link">
+              <span class="mobile-menu__title">新規登録</span>
+            </a>
+          </li>
+          <li class="mobile-menu__item">
+            <a href="login.php" class="mobile-menu__link">
+              <span class="mobile-menu__title">ログイン</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+
     <!----------------------------------------------->
     <!-- javascript はここから -->
-    <script
-      src="https://code.jquery.com/jquery-3.3.1.min.js"
-      integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-      integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-      integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-      crossorigin="anonymous"
-    ></script>
-    <script src="js/main.js"></script>
+      <script
+        src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"
+      ></script>
+      <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"
+      ></script>
+      <script
+        src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"
+      ></script>
+      <script src="js/main.js"></script>
     <!----------------------------------------------->
+    </div>
   </body>
 </html>
--- js/subpage.js
+++ js/subpage.js
@@ -1,12 +1,3 @@
-$(function() {
-	setTimeout(function(){
-		$('.start-inner').fadeIn(1600);
-	},500); //0.5秒後にロゴをフェードイン!
-	setTimeout(function(){
-		$('.start').fadeOut(500);
-	},1500); //1.5秒後にロゴ含め真っ白背景をフェードアウト！
-});
-
 class MobileMenu {
 	constructor() {
 		this.DOM = {}
